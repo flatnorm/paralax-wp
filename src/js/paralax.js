53ba7f0a8417aa70e54844b698bf05a82a566bb2
hudsoncliff <hudsonlciffs1999@gmail.com>
2020-06-17 11:02:06 +0900

    パララックス調整
    原因は画像遅延が濃厚、つまりテーマの仕組み。

--- src/js/paralax.js
+++ src/js/paralax.js
@@ -1,20 +1,23 @@
 (function($) {
 
     var parallax = -0.5;
-    // var parallax = -0.25; //ここの値をと画像のサイズを変更しながら調整
 
     var $bg_images = $('.p-section-header__upper'); //パララックス効果を与えるクラスを指定
-    var offset_tops = [];
-    $bg_images.each(function(i, el) {
-        offset_tops.push($(el).offset().top);
-    });
 
     $(window).scroll(function() {
+
+        var offset_tops = [];
+        var clientHeights = [];
+        $bg_images.each(function(i, el) {
+            offset_tops.push($(el).offset().top);
+            clientHeights.push($(el).outerHeight());
+            console.dir($(el).offset().top);
+        });
+        
         var dy = $(this).scrollTop();
         $bg_images.each(function(i, el) {
         var ot = offset_tops[i];
         var slideHeight = ( ot - dy ) / 4 * parallax;
-        console.dir(slideHeight);
         $(el).css('background-position', 'center ' + slideHeight + 'px');
         });
     });
